f97d06055046c7d3dff8cf5514f2867b0ff7f6c3
Steve Gossip <stephengossip@gmail.com>
2019-06-08 17:55:20 +0100

    brand-select working on new shoe page

--- controllers/shoe_controller.rb
+++ controllers/shoe_controller.rb
@@ -2,24 +2,32 @@ require('sinatra')
 require('sinatra/contrib/all')
 
 require_relative('../models/shoe')
+require_relative('../models/brand')
 
 also_reload('../models/*')
+
+#INDEX
 
 get '/shoes' do
   @shoes = Shoe.all()
   erb(:"shoes/index")
 end
 
+#NEW
+
+get '/shoes/new' do
+  @brands = Brand.all()
+  erb(:"shoes/new")
+end
+
+#SHOW
+
 get '/shoes/:id' do
   @shoe = Shoe.find_by_id(params['id'].to_i)
   erb(:"shoes/show")
 end
 
-get 'shoes/new' do
-  # @shoe = Shoe.new(params)
-  # @shoe.save()
-  erb(:"shoes/new")
-end
+#DELETE
 
 post '/shoes/:id/delete' do
   @shoe = Shoe.find_by_id(params['id'].to_i)
