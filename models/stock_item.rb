592270a36351fc2f31420ae32ec6771683ded206
Steve Gossip <stephengossip@gmail.com>
2019-06-10 15:41:20 +0100

    save and self.all functions working in stock_item.rb

--- models/stock_item.rb
+++ models/stock_item.rb
@@ -12,5 +12,29 @@ class StockItem
     @quantity = options['quantity'].to_i
     @shoe_id = options['shoe_id'].to_i
   end
-  
+
+#CREATE
+
+  def save()
+    sql = "INSERT INTO stock_items
+    (
+      quantity, shoe_id
+    )
+    VALUES
+    (
+      $1, $2
+    ) RETURNING * "
+    values = [@quantity, @shoe_id]
+    result = SqlRunner.run(sql, values).first
+    @id = result['id'].to_i
+  end
+
+#READ
+
+  def self.all()
+    sql = "SELECT * FROM stock_items"
+    stock_items = SqlRunner.run(sql)
+    stock_items.map { |stock_item| StockItem.new(stock_item)}
+  end
+
 end
